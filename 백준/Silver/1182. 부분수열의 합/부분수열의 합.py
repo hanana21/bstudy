f29aa0e6d9c6b73518eca378b676d31775b084c0
이하나 <134573117+hanana21@users.noreply.github.com>
2024-01-18 15:17:47 +0900

    [Silver II] Title: 부분수열의 합, Time: 368 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -7,7 +7,7 @@ array = list(map(int,input().split()))
 count = 0
 
 for i in range(1,n+1):
-    combi = list(combinations(array,i))
+    combi = combinations(array,i) #튜플 형태의 리스트 반환  
     for j in combi:
         if sum(j) == s: count += 1
 print(count)
