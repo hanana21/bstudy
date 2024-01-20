651ce85edadf9844dd722d8b3066f2beb4892878
이하나 <134573117+hanana21@users.noreply.github.com>
2024-01-20 17:06:21 +0900

    [Silver I] Title: 트리 순회, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,32 @@
+n=int(input())     
+tree ={}
+for _ in range(n):
+    root,left,right = input().split()
+    tree[root] = [left,right]
+
+def preorder(root):
+    print(root,end='')
+    if tree[root][0] != '.':
+        preorder(tree[root][0]) 
+    if tree[root][1] != '.':
+        preorder(tree[root][1])
+    return
+
+def inorder(root):
+    if tree[root][0] != '.':
+        inorder(tree[root][0])
+    print(root,end='')
+    if tree[root][1] != '.':
+        inorder(tree[root][1])
+    
+def postorder(root):
+    if tree[root][0] !='.':
+        postorder(tree[root][0])
+    if tree[root][1] !='.':
+        postorder(tree[root][1])
+    print(root,end='')
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
