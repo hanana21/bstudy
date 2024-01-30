6336994ae19e66580344a0bb2387161e84915efd
이하나 <134573117+hanana21@users.noreply.github.com>
2024-01-30 22:24:08 +0900

    [Silver I] Title: 회의실 배정, Time: 4036 ms, Memory: 47804 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+#회의실 배정하기
+N = int(input())
+times= []
+for i in range(N):
+    start,end = map(int,input().split())
+    times.append([start,end])
+times.sort(key = lambda x: x[0])
+times.sort(key = lambda x: x[1])
+
+cnt = 1
+end_time = times[0][1]
+for i in range(1,N):
+    if times[i][0] >= end_time:
+        cnt += 1
+        end_time = times[i][1]
+print(cnt)
+
+
+
+
