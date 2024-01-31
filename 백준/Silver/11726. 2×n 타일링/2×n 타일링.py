856267da0a98db5bca57deb242a79e2e0f274612
이하나 <134573117+hanana21@users.noreply.github.com>
2024-01-31 17:18:35 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,12 @@
 # 2xN 타일링
+import sys
+input = sys.stdin.readline
 n = int(input())
 
-dp = [0] * (n+1)
-dp[0] = 1
-dp[1] = 2
+dp = [0] * (n+4)
+dp[1] = 1
+dp[2] = 2
 
-for i in range(2,n+1):
+for i in range(3,n+1):
     dp[i] = dp[i-1] + dp[i-2]
-print(dp[n-1]%10007)
+print(dp[n]%10007)
